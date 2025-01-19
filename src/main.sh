90f8d521a20e0790f1335c1dfdf82e608c8f4fe8
Russell <russell.busch@gmail.com>
2025-01-19 09:23:18 -0600

    Refactored playback speed to a variable. May add it to config later for easy changing.

--- src/main.sh
+++ src/main.sh
@@ -3,6 +3,7 @@
 video_dir="$HOME/.local/share/aerial-wayland/videos"
 # read timeout (in minutes) from a plain text file 'aerial.config' and multiply by 60 to get seconds
 timeout=$(grep "timeout=" "$HOME/.local/share/aerial-wayland/aerial.config" | cut -d'=' -f2)
+playback_speed=0.2
 timeout=$((timeout * 60))
 
 cleanup() {
@@ -14,7 +15,7 @@ cleanup() {
 
 trap cleanup SIGTERM SIGINT
 
-cmd="mpv --no-audio --really-quiet --speed=0.5 --stop-screensaver=no --panscan=1.0 -fs --shuffle --playlist='$video_dir' & disown"
+cmd="mpv --no-audio --really-quiet --speed=$playback_speed --stop-screensaver=no --panscan=1.0 -fs --shuffle --playlist='$video_dir' & disown"
 
 # Run swayidle without exec to allow trap to work
 swayidle -w \
