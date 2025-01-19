c916729f32d40a0c47dd2f3a6b15f0887b5b80fc
Russell <russell.busch@gmail.com>
2025-01-19 10:49:34 -0600

    Added mpv flags to smooth out slower playback.

--- src/main.sh
+++ src/main.sh
@@ -3,8 +3,8 @@
 video_dir="$HOME/.local/share/aerial-wayland/videos"
 # read timeout (in minutes) from a plain text file 'aerial.config' and multiply by 60 to get seconds
 timeout=$(grep "timeout=" "$HOME/.local/share/aerial-wayland/aerial.config" | cut -d'=' -f2)
-playback_speed=0.2
 timeout=$((timeout * 60))
+playback_speed=0.5
 
 cleanup() {
     pkill mpv
@@ -15,12 +15,13 @@ cleanup() {
 
 trap cleanup SIGTERM SIGINT
 
-cmd="mpv --no-audio --really-quiet --speed=$playback_speed --stop-screensaver=no --panscan=1.0 -fs --shuffle --playlist='$video_dir' & disown"
+cmd="mpv --no-audio --no-osc --framedrop=no --video-sync=display-resample --really-quiet --speed=$playback_speed --stop-screensaver=no --panscan=1.0 -fs --shuffle --playlist='$video_dir' & disown"
 
-# Run swayidle without exec to allow trap to work
 swayidle -w \
     timeout $timeout "$cmd" \
-    resume 'pkill mpv' &
+    resume 'pkill mpv' \
+    before-sleep 'pkill mpv' \
+    lock 'pkill mpv' &
 
 # Wait indefinitely while still allowing signals to be caught
 wait
